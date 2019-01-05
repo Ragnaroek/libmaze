5b8764b6549f242978cdccbc3c9810a148eb372d
Michael Bohn <michael.bohn@freiheit.com>
2019-01-05 13:31:00 +0100

    write plm data

--- src/plm.rs
+++ src/plm.rs
@@ -4,7 +4,7 @@ use std::io;
 use std::io::Write;
 use std::fs::OpenOptions;
 
-use super::square_maze::{SquareMaze};
+use super::square_maze::{SquareMaze, WallDirection};
 
 /// plm = _p_ortable _l_abyrinth for_m_at
 pub fn output(path: &Path, maze: &SquareMaze) -> io::Result<()> {
@@ -23,8 +23,54 @@ pub fn output(path: &Path, maze: &SquareMaze) -> io::Result<()> {
                      maze.entry.x as u8, maze.entry.y as u8,
                      maze.exit.x as u8, maze.exit.y as u8])?;
 
-    //TODO Write labyrinth layout bits!!!!
-    
+    let num_bits = 2 * ((maze.width + 1) * (maze.height + 1));
+    let num_bytes = f32::ceil(num_bits as f32 / 8.0) as usize;
+
+    let mut data = vec![0; num_bytes];
+
+    let mut bit_cnt = 0;
+    let mut byte_cnt = 0;
+    let mut wall_byte = data[0];
+
+    for y in 0..maze.height + 1 {
+        for x in 0..maze.width + 1 {
+
+            if x == maze.width && y == maze.height {
+                //no-op, write zeros for east and south
+            } else if x == maze.width {
+                if maze.wall(WallDirection::EAST, x-1, y) {
+                    wall_byte = wall_byte | (1 << bit_cnt);
+                }
+            } else if y == maze.height {
+                if maze.wall(WallDirection::SOUTH, x, y-1) {
+                    wall_byte = wall_byte | (1 << (bit_cnt + 1));
+                }
+            } else {
+                if maze.wall(WallDirection::WEST, x, y) {
+                    wall_byte = wall_byte | (1 << bit_cnt);
+                }
+
+                if maze.wall(WallDirection::NORTH, x, y) {
+                    wall_byte = wall_byte | (1 << (bit_cnt + 1));
+                }
+            }
+
+            if bit_cnt >= 6 {
+                data[byte_cnt] = wall_byte;
+                bit_cnt = 0;
+                byte_cnt = byte_cnt + 1;
+                if byte_cnt >= num_bytes {
+                    break;
+                } else {
+                    wall_byte = data[byte_cnt];
+                }
+            } else {
+                bit_cnt = bit_cnt + 2;
+            }
+        }
+    }
+
+    file.write_all(&data)?;
     return Ok(())
 }
 
